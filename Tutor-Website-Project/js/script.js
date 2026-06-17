ccfd29881509a39bf8e8277ea8700ead37c6e662
Ridwan2024-P <ahsanrid@gmail.com>
2026-06-17 13:39:56 +0600

    Navbar-Background-On-Scroll

--- Tutor-Website-Project/js/script.js
+++ Tutor-Website-Project/js/script.js
@@ -1,3 +1,6 @@
+
+
+//Toggle mobile menu
 document.addEventListener('DOMContentLoaded' ,function () {
     const toggleButton = document.querySelector('.navbar .mobile-menu-toggle');
     const mobileMenu = document.querySelector('.navbar .mobile-menu-items');
@@ -7,3 +10,14 @@ document.addEventListener('DOMContentLoaded' ,function () {
     })
 
 })
+
+// change navbar background on scroll
+window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.navbar');
+
+    if (window.scrollY > 0) {
+        navbar.classList.add('navbar-scroll');
+    } else {
+        navbar.classList.remove('navbar-scroll');
+    }
+});
